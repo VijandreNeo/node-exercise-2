e02f9907371aecc74c42e3f1db53cf9a40acae26
Neo Jan Erich Vijandre <neojanerich.vijandre@neojanerich.vijandre-CH4K3HF5WM>
2022-10-18 13:43:49 +0800

    Modified for readability

--- bio.js
+++ bio.js
@@ -19,6 +19,7 @@ function readCSV(inputFile) {
     delimiter: ',',
     quote: '"',
   }
+
   return toObject(data, config)
 }
 
@@ -42,6 +43,7 @@ function writeCSV(inputPath, bioArray) {
 
 function createBio(fromCSV, newBio) {
   const find = fromCSV.find((csvBio) => csvBio.name.toUpperCase() === newBio.name.toUpperCase())
+
   if (find === undefined) {
     fromCSV.push(newBio)
     return fromCSV
@@ -51,6 +53,7 @@ function createBio(fromCSV, newBio) {
 
 function readBio(fromCSV, newBio) {
   const find = fromCSV.find((csvBio) => csvBio.name.toUpperCase() === newBio.name.toUpperCase())
+
   if (find === undefined) {
     return null
   }
@@ -59,6 +62,7 @@ function readBio(fromCSV, newBio) {
 
 function updateBio(fromCSV, newBio) {
   let checkerIfExist = false
+
   fromCSV.forEach((csvBio) => {
     if (csvBio.name.toUpperCase() === newBio.name.toUpperCase()) {
       Object.assign(csvBio, newBio)
@@ -73,86 +77,97 @@ function updateBio(fromCSV, newBio) {
 
 function deleteBio(fromCSV, newBio) {
   const find = fromCSV.find((csvBio) => csvBio.name.toUpperCase() === newBio.name.toUpperCase())
+
   if (find === undefined) {
     return null
   }
   const newArr = fromCSV.filter((csvBio) => csvBio.name.toUpperCase() !== newBio.name.toUpperCase())
+
   return newArr
 }
 
 try {
   if (options === undefined && inputName === undefined) {
     throw Error('Option or Name not specified')
-  } else {
-    const fromCSV = readCSV('biostats.csv')
-    switch (options.toLowerCase()) {
-      case '-c': case '-u':
-        if (inputSex.toLowerCase() !== 'f' && inputSex.toLowerCase() !== 'm') {
-          throw Error('Incorrect Sex')
-        } else if (Number.isNaN(parseInt(inputAge, 10)) || parseInt(inputAge, 10) < 18) {
-          throw Error('Age not a number or underaged')
-        } else if (Number.isNaN(parseInt(inputHeight, 10))) {
-          throw Error('Height not a number')
-        } else if (Number.isNaN(parseInt(inputWeight, 10))) {
-          throw Error('Weight not a number')
-        } else {
-          const nameUpperCase = inputName[0].toUpperCase() + inputName.substring(1).toLowerCase()
-          const sexUpperCase = inputSex.toUpperCase()
-          const ageInt = parseInt(inputAge, 10)
-          const heightInt = parseInt(inputHeight, 10)
-          const weightInt = parseInt(inputWeight, 10)
-          const stats = new Bio(nameUpperCase, sexUpperCase, ageInt, heightInt, weightInt)
+  }
+  const fromCSV = readCSV('biostats.csv')
+  switch (options.toLowerCase()) {
+    case '-c': case '-u': {
+      const sexUpperCase = inputSex.toUpperCase()
+      const ageInt = parseInt(inputAge, 10)
+      const heightInt = parseInt(inputHeight, 10)
+      const weightInt = parseInt(inputWeight, 10)
 
-          if (options.toLowerCase() === '-c') {
-            const result = createBio(fromCSV, stats)
-            if (result === null) {
-              throw Error('Name already exists')
-            }
-            const alteredArr = writeCSV('biostats.csv', result)
-            if (alteredArr === null) {
-              throw Error('File does not exist')
-            }
-          } else if (options.toLowerCase() === '-u') {
-            const result = updateBio(fromCSV, stats)
-            if (result === null) {
-              throw Error('Name does not exist')
-            }
-            const alteredArr = writeCSV('biostats.csv', result)
-            if (alteredArr === null) {
-              throw Error('File does not exist')
-            }
-          }
-        }
-        break
-      case '-d': case '-r': {
-        if (inputName === undefined) {
-          throw Error('No name specified')
-        }
-        const stats = new Bio(inputName.toUpperCase())
-        if (options === '-d') {
-          const result = deleteBio(fromCSV, stats)
-          if (result === null) {
-            throw Error('Name does not exist')
-          }
-          const alteredArr = writeCSV('biostats.csv', result)
-          if (alteredArr === null) {
-            throw Error('File does not exist')
-          }
-        } else if (options === '-r') {
-          const result = readBio(fromCSV, stats)
-          if (result === null) {
-            throw Error('Name does not exist')
-          }
-          console.log(`\nName: ${result.name} \n 
-          Sex: ${result.sex === 'F' ? 'FEMALE' : 'MALE'} \n
-          Age: ${result.age} \n
-          Height (inches): ${result.height} (centimeters): ${result.height *= 2.54} \n
-          Weight (pounds): ${result.weight} (kilos): ${result.weight *= 0.45359237}\n`)
-        }
-        break
+      if (sexUpperCase !== 'F' && sexUpperCase !== 'M') {
+        throw Error('Incorrect Sex')
+      } else if (Number.isNaN(ageInt) || ageInt < 18) {
+        throw Error('Age not a number or underaged')
+      } else if (Number.isNaN(heightInt)) {
+        throw Error('Height not a number')
+      } else if (Number.isNaN(weightInt)) {
+        throw Error('Weight not a number')
       }
-      default:
-        throw Error('Wrong option argument')
+      const nameUpperCase = inputName[0].toUpperCase() + inputName.substring(1).toLowerCase()
+      const stats = new Bio(nameUpperCase, sexUpperCase, ageInt, heightInt, weightInt)
+
+      if (options.toLowerCase() === '-c') {
+        const result = createBio(fromCSV, stats)
+
+        if (result === null) {
+          throw Error('Name already exists')
+        }
+        const writeResult = writeCSV('biostats.csv', result)
+
+        if (writeResult === null) {
+          throw Error('File does not exist')
+        }
+      } else if (options.toLowerCase() === '-u') {
+        const result = updateBio(fromCSV, stats)
+
+        if (result === null) {
+          throw Error('Name does not exist')
+        }
+        const writeResult = writeCSV('biostats.csv', result)
+        if (writeResult === null) {
+          throw Error('File does not exist')
+        }
+      }
+      break
     }
+    case '-d': case '-r': {
+      if (inputName === undefined) {
+        throw Error('No name specified')
+      }
+      const stats = new Bio(inputName.toUpperCase())
+
+      if (options === '-d') {
+        const result = deleteBio(fromCSV, stats)
+
+        if (result === null) {
+          throw Error('Name does not exist')
+        }
+        const writeResult = writeCSV('biostats.csv', result)
+
+        if (writeResult === null) {
+          throw Error('File does not exist')
+        }
+      } else if (options === '-r') {
+        const result = readBio(fromCSV, stats)
+
+        if (result === null) {
+          throw Error('Name does not exist')
+        }
+        console.log(`
+          Name: ${result.name}
+          Sex: ${result.sex === 'F' ? 'FEMALE' : 'MALE'}
+          Age: ${result.age}
+          Height (inches): ${result.height} (centimeters): ${result.height *= 2.54} 
+          Weight (pounds): ${result.weight} (kilos): ${result.weight *= 0.45359237}
+          `)
+      }
+      break
+    }
+    default:
+      throw Error('Wrong option argument')
   }
 } catch (error) { console.error('Error: ', error) }
